fc29008a4590fa56fa13dca7ae895f3751efec9b
Jozsa Csongor <jozsa.csongor@gmail.com>
2020-02-17 19:17:30 +0100

    add some comments

--- start-django-project.sh
+++ start-django-project.sh
@@ -7,17 +7,21 @@ else
     APP_NAME=$1
 fi
 
+# set up app name in config files
 sed -i "s/my_app/${APP_NAME}/g" Procfile 
 sed -i "s/my_app/${APP_NAME}/g" .env.example
 cp -i .env.example .env
 
+# start new django project
 django-admin startproject ${APP_NAME}
 
+# move django app to root dir
 mv "${APP_NAME}/manage.py" ./
 mv "${APP_NAME}/${APP_NAME}" ./app
 rmdir "${APP_NAME}"
 mv ./app "${APP_NAME}"
 
+# separate settings for dev and prod environments
 mkdir ${APP_NAME}/settings/
 touch ${APP_NAME}/settings/__init__.py
 mv ${APP_NAME}/settings.py ${APP_NAME}/settings/base.py
