8cd4a960142f82a830ce1de874d7e4b4caacd364
Jozsa Csongor <jozsa.csongor@gmail.com>
2020-02-17 19:03:54 +0100

    settings

---
+++ settings.dev.py
@@ -0,0 +1,10 @@
+from .base import *
+
+DEBUG = True
+
+ALLOWED_HOSTS = ['*']
+
+SECRET_KEY = 'secret'
+
+MEDIA_URL = '/media/'
+STATIC_URL = '/static/'
--- start-django-project.sh
+++ start-django-project.sh
@@ -10,7 +10,16 @@ fi
 sed -i "s/my_app/${APP_NAME}/g" Procfile 
 sed -i "s/my_app/${APP_NAME}/g" .env.example
 cp -i .env.example .env
-django-admin startproject ${APP_NAME}
-mv ${APP_NAME}/* ./
-rmdir ${APP_NAME}
 
+django-admin startproject ${APP_NAME}
+
+tmp=`mktemp -d`
+mv "${APP_NAME}/"* "${tmp}"
+rmdir "${APP_NAME}"
+mv "${tmp}" "${APP_NAME}"
+
+mkdir ${APP_NAME}/${APP_NAME}/settings/
+touch ${APP_NAME}/${APP_NAME}/settings/__init__.py
+mv ${APP_NAME}/${APP_NAME}/settings.py ${APP_NAME}/${APP_NAME}/settings/base.py
+mv settings.prod.py ${APP_NAME}/${APP_NAME}/settings/prod.py
+mv settings.dev.py ${APP_NAME}/${APP_NAME}/settings/dev.py
