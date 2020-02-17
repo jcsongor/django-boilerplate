89adffe7fea4b564c473f974636d5b7eea0124d0
Jozsa Csongor <jozsa.csongor@gmail.com>
2020-02-17 19:15:57 +0100

    fix starting django project

--- start-django-project.sh
+++ start-django-project.sh
@@ -13,13 +13,13 @@ cp -i .env.example .env
 
 django-admin startproject ${APP_NAME}
 
-tmp=`mktemp -d`
-mv "${APP_NAME}/"* "${tmp}"
+mv "${APP_NAME}/manage.py" ./
+mv "${APP_NAME}/${APP_NAME}" ./app
 rmdir "${APP_NAME}"
-mv "${tmp}" "${APP_NAME}"
+mv ./app "${APP_NAME}"
 
-mkdir ${APP_NAME}/${APP_NAME}/settings/
-touch ${APP_NAME}/${APP_NAME}/settings/__init__.py
-mv ${APP_NAME}/${APP_NAME}/settings.py ${APP_NAME}/${APP_NAME}/settings/base.py
-mv settings.prod.py ${APP_NAME}/${APP_NAME}/settings/prod.py
-mv settings.dev.py ${APP_NAME}/${APP_NAME}/settings/dev.py
+mkdir ${APP_NAME}/settings/
+touch ${APP_NAME}/settings/__init__.py
+mv ${APP_NAME}/settings.py ${APP_NAME}/settings/base.py
+mv settings.prod.py ${APP_NAME}/settings/prod.py
+mv settings.dev.py ${APP_NAME}/settings/dev.py
