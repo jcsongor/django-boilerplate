26242ac096c57ac4e140af7ce83ca2daa6e3adec
Józsa Csongor <jozsa.csongor@gmail.com>
2024-01-18 22:14:14 +0100

    Add DB settings for local dev

--- settings.dev.py
+++ settings.dev.py
@@ -8,3 +8,13 @@ SECRET_KEY = 'secret'
 
 MEDIA_URL = '/media/'
 STATIC_URL = '/static/'
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'dev',
+        'USER': 'dev',
+        'PASSWORD': 'dev',
+        'HOST': 'db',
+        'PORT': 5432,
+    }
+}
